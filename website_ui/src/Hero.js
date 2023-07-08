ea3b17cf122c8626a2861b6c401460190436428c
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-08 13:42:20 -0500

    Hero

---
+++ website_ui/src/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Welcome to Our Website</h1>
+      <p>Discover our amazing services and features.</p>
+    </section>
+  );
+}
+
+export default Hero;
