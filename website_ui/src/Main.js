fa132ac6c556a6240857ec0d1cf68aa34a421bf5
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-08 13:42:51 -0500

    Main Component

---
+++ website_ui/src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <main className="main-content">
+      <h2>Our Services</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </main>
+  );
+}
+
+export default Main;
